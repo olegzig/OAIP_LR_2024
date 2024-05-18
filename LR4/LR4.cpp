b3f2cfe34cb3b6e0f026f7f19ba22c8d65397a45
olegzig <boris37544@gmail.com>
2024-05-18 14:39:07 +0300

    Добавьте файлы проекта.

---
+++ LR4/LR4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+const char* strShell(const char* str1, const char* str2) {
+    return strstr(str1, str2);
+}
+
+const char* strImitate(const char* str1, const char* str2) {
+    char* cp = (char*)str1;
+    char* s1, * s2;
+
+    if (!*str2)
+        return((char*)str1);
+
+    while (*cp)
+    {
+        s1 = cp;
+        s2 = (char*)str2;
+
+        while (*s1 && *s2 && !(*s1 - *s2))
+            s1++, s2++;
+
+        if (!*s2)
+            return(cp);
+
+        cp++;
+    }
+
+    return(NULL);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
